78d3d8a573bf0fdaa5b0e1e02c3431ca6f45901a
tranhoangminh <hoangminhcth1@gmail.com>
2018-07-16 06:22:23 +0700

    Update

--- 1.js
+++ 1.js
@@ -1,3 +1,12 @@
 $(function(){
-
+	$(window).resize(function(){
+		var checksize = $(window).width();
+		(checksize <= 1024) ? ($('#container').removeClass('container')) : ($('#container').addClass('container'));	
+	});
+	
+	if($(document).width() <= 1024){
+		$('#container').removeClass('container');
+	}else{
+		$('#container').addClass('container');
+	}	
 })
